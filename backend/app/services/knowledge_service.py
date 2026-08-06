58758db43f52643ca56c4065d3add5d6900d0f8d
AbhiNair6752 <abhinair6752@gmail.com>
2026-08-06 09:49:30 +0530

    the knowledge service has been refactored

--- backend/app/services/knowledge_service.py
+++ backend/app/services/knowledge_service.py
@@ -9,31 +9,60 @@ class KnowledgeService:
             self,
             question: str
     )-> DocumentContext:
+        
+        """
+    Selects the most relevant document
+    for the user's question and loads
+    all its chunks.
+    """
+        documents = self._select_documents(question)
+        
+        return self._load_document(documents[0])
+    
+    def _load_document(
+            self,
+            filename: str,
+    
+    ) -> DocumentContext:
+        chunks = vector_store.get_document_chunks(
+            filename
+        )
+        return DocumentContext(
+            filename=filename,
+            chunks=chunks
+        )
+    
+    def _select_documents(
+        self,
+        question: str,
+    )-> list[str]:
         available_documents = vector_store.get_uploaded_documents()
 
         selection = document_selector.select(
             question=question,
             available_documents=available_documents
         )
+
         if selection.status == "not_found":
             raise DocumentNotFoundException(
-                "No Matching document found"
+                "No matching document found"
             )
-        
         if selection.status == "ambiguous":
             raise AmbiguousDocumebtException(
-                "Multiple matching documents found"
+                "Multiple matching documents are found"
             )
+        return selection.documents
+    
+    def get_documents(
+            self,
+            question: str,
+    ) -> list[DocumentContext]:
         
-        filename = selection.documents[0]
+        documents = self._select_documents(question)
 
-        chunks = vector_store.get_document_chunks(
-            filename
-        )
-
-        return DocumentContext(
-            filename=filename,
-            chunks=chunks
-        )
+        return [
+            self._load_document(doc)
+            for doc in documents
+        ]
 
 knowledge_service = KnowledgeService()
